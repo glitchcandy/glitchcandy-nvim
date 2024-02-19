d320b3bbd80aede06c16862cd7739e0e8d9e755f
Gruficlath <glitchycrafting@gmail.com>
2024-02-19 10:43:34 -0600

    Fix lualine

--- lua/lualine/themes/glitchcandy_nvim.lua
+++ lua/lualine/themes/glitchcandy_nvim.lua
@@ -3,36 +3,36 @@ local colors = require("glitchcandy_nvim").colors()
 local bg = colors.bg_alt
 
 local normal = {
-   a = { fg = colors.black, bg = colors.purple, gui = 'bold' },
+   a = { fg = colors.bg, bg = colors.purple, gui = 'bold' },
    b = { fg = colors.purple, bg = bg },
-   c = { fg = colors.white, bg = bg },
+   c = { fg = colors.fg, bg = bg },
 }
 
 local command = {
-   a = { fg = colors.black, bg = colors.cyan, gui = 'bold' },
+   a = { fg = colors.fb, bg = colors.cyan, gui = 'bold' },
    b = { fg = colors.cyan, bg = bg },
 }
 
 local visual = {
-   a = { fg = colors.black, bg = colors.pink, gui = 'bold' },
+   a = { fg = colors.bg, bg = colors.pink, gui = 'bold' },
    b = { fg = colors.pink, bg = bg },
 }
 
 local inactive = {
-   a = { fg = colors.white, bg = colors.visual, gui = 'bold' },
-   b = { fg = colors.black, bg = colors.white },
+   a = { fg = colors.fg, bg = colors.orange, gui = 'bold' },
+   b = { fg = colors.bg, bg = colors.fg },
 }
 
 local replace = {
-   a = { fg = colors.black, bg = colors.yellow, gui = 'bold' },
+   a = { fg = colors.bg, bg = colors.yellow, gui = 'bold' },
    b = { fg = colors.yellow, bg = bg },
-   c = { fg = colors.white, bg = bg },
+   c = { fg = colors.fg, bg = bg },
 }
 
 local insert = {
-   a = { fg = colors.black, bg = colors.green, gui = 'bold' },
+   a = { fg = colors.bg, bg = colors.green, gui = 'bold' },
    b = { fg = colors.green, bg = bg },
-   c = { fg = colors.white, bg = bg },
+   c = { fg = colors.fg, bg = bg },
 }
 
 return {
