bdacb6e3659f18bde108b8137a5f316ac36a84cf
Gruficlath <glitchycrafting@gmail.com>
2024-02-20 21:58:52 -0600

    Revert "Rolling back, didn't fix"
    This reverts commit 842e0f6a350e0436bc2546a2344886d4bd4d357d.

--- lua/glitchcandy/groups.lua
+++ lua/glitchcandy/groups.lua
@@ -365,6 +365,15 @@ local function setup()
 		CmpItemKindConstant = { link = "@constant" },
 		CmpItemKindStruct = { link = "@type" },
 		CmpItemKindTypeParameter = { link = "@variable.parameter" },
+
+		TSTitle = { link = "markdownH1", },
+		TSLiteral = { link = "markdownBlockquote", },
+		TSEmphasis = { link = "markdownItalic", },
+		TSStrong = { link = "markdownBold", },
+		TSURI = { link = "markdownLinkText", },
+		TSTextReference = { link = "markdownId", },
+		TSPuncSpecial = { link = "markdownRule", },
+		TSStringEscape = { link = "markdownRule", },
 	}
 end
 
